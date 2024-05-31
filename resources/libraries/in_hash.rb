7c05762136cf986125a79cd0bcda500aba171308
Miguel Negron <manegron@redborder.com>
2024-05-31 10:31:36 +0100

    lint libraries 2

--- resources/libraries/in_hash.rb
+++ resources/libraries/in_hash.rb
@@ -4,7 +4,7 @@ class Zookeeper
       if hash[keys.first] && keys.size == 1
         return true
       elsif hash[keys.first] && hash[keys.first].is_a?(Hash)
-        in_hash? hash[keys.first], *keys[1..keys.size-1]
+        in_hash? hash[keys.first], *keys[1..keys.size - 1]
       else
         return false
       end
